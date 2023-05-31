d18b473c727d4c799c82369e18179c19b41f0bb7
kaia <89624646+kaiakm@users.noreply.github.com>
2023-05-30 22:20:28 -0500

    pseudocode

---
+++ tictactoe.rb
@@ -0,0 +1,29 @@
+class TicTacToe
+    # contructor to initialize game
+    def intialize
+    end
+
+    #function to display the game board
+    def display_board
+    end
+
+    #function to check for a win
+    def check_win(player)
+    end
+
+    #function to check for a tie
+    def check_tie
+    end
+
+    #function to update board with player's move
+    def update_board(position)
+    end
+
+    #function to switch player
+    def switch_player
+    end
+
+    #function for game logic
+    def play_game
+    end
+end
